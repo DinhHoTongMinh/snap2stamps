61d83b3b62f78b648f0a93ff187c660a6c38fadf
DinhHoTongMinh <96289487+DinhHoTongMinh@users.noreply.github.com>
2021-12-17 15:34:42 +0100

    work for python 3

--- bin/slaves_prep.py
+++ bin/slaves_prep.py
@@ -1,4 +1,4 @@
-### Python script to use SNAP as InSAR processor compatible with StaMPS PSI processing
+# Python script to use SNAP as InSAR processor compatible with StaMPS PSI processing
 # Author Jose Manuel Delgado Blasco
 # Date: 21/06/2018
 # Version: 1.0
@@ -30,7 +30,7 @@ try:
         for line in in_file.readlines():
                 if "PROJECTFOLDER" in line:
                         PROJECT = line.split('=')[1].strip()
-                        print PROJECT
+                        print(PROJECT) 
 finally:
         in_file.close()
 
@@ -51,14 +51,14 @@ for filename in os.listdir(directory):
     if filename.endswith(".zip") : 
        print(os.path.join(directory, filename))
        head, tail = os.path.split(os.path.join(directory, filename))
-       print tail[17:25]
+       print(tail[17:25]) 
        subdirectory=directory+'/'+tail[17:25]
        if not os.path.exists(subdirectory):
-		os.makedirs(subdirectory)
+           os.makedirs(subdirectory)
        #### Moving files
        source=os.path.join(directory, filename)
        destination=os.path.join(subdirectory, tail)
-       print 'Moving '+source+' to '+destination
+       print('Moving '+source+' to '+destination) 
        shutil.move(source,destination)
     else:
         continue
--- bin/stamps_export.py
+++ bin/stamps_export.py
@@ -28,23 +28,23 @@ try:
         for line in in_file.readlines():
                 if "PROJECTFOLDER" in line:
                         PROJECT = line.split('=')[1].strip()
-                        print PROJECT
+                        print(PROJECT)
                 if "IW1" in line:
                         IW = line.split('=')[1].strip()
-                        print IW
-		if "MASTER" in line:
-			MASTER = line.split('=')[1].strip()
-			print MASTER
+                        print(IW)
+                if "MASTER" in line:
+                    MASTER = line.split('=')[1].strip()
+                    print(MASTER)
                 if "GRAPHSFOLDER" in line:
                         GRAPH = line.split('=')[1].strip()
-                        print GRAPH
+                        print(GRAPH) 
                 if "GPTBIN_PATH" in line:
                         GPT = line.split('=')[1].strip()
-                        print GPT
-		if "CACHE" in line:
-			CACHE = line.split('=')[1].strip()
-		if "CPU" in line:
-			CPU = line.split('=')[1].strip()
+                        print(GPT) 
+                if "CACHE" in line:
+                    CACHE = line.split('=')[1].strip()
+                if "CPU" in line:
+                    CPU = line.split('=')[1].strip()
 finally:
         in_file.close()
 
@@ -67,12 +67,12 @@ out_file = open(outlog, 'a')
 err_file=out_file
 graphxml=GRAPH+'/export.xml'
 graph2run=GRAPH+'/export2run.xml'
-print bar_message
+print(bar_message) 
 out_file.write(bar_message)
 message='## StaMPS PSI export started:\n'
-print message
+print(message) 
 out_file.write(message)
-print bar_message
+print(bar_message) 
 out_file.write(bar_message)
 k=0
 for dimfile in glob.iglob(coregfolder + '/*'+IW+'.dim'):
@@ -80,37 +80,38 @@ for dimfile in glob.iglob(coregfolder + '/*'+IW+'.dim'):
     k=k+1
     message='['+str(k)+'] Exporting pair: master-slave pair '+tail+'\n'
     ifgdim = Path(ifgfolder+'/'+tail)
-    print ifgdim
+    print(ifgdim) 
     if ifgdim.is_file():
-        print message
+        print(message) 
         out_file.write(message)
-	with open(graphxml, 'r') as file :
-           filedata = file.read()
-        # Replace the target string
-	filedata = filedata.replace('COREGFILE',dimfile)
-	filedata = filedata.replace('IFGFILE', str(ifgdim))
-	filedata = filedata.replace('OUTPUTFOLDER',outputexportfolder)
-	# Write the file out again
-	with open(graph2run, 'w') as file:
-           file.write(filedata)
-        args = [ GPT, graph2run, '-c', CACHE, '-q', CPU]
-	print args
-	# Launching process
-        process = subprocess.Popen(args, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-	timeStarted = time.time()
-        stdout = process.communicate()[0]
-        print 'SNAP STDOUT:{}'.format(stdout)
-        timeDelta = time.time() - timeStarted                     # Get execution time.
-        print('['+str(k)+'] Finished process in '+str(timeDelta)+' seconds.')
-	out_file.write('['+str(k)+'] Finished process in '+str(timeDelta)+' seconds.\n')
-        if process.returncode != 0 :
-           message='Error exporting '+str(tail)+'\n' 
-           err_file.write(message)
-        else:
-           message='Stamps export of '+str(tail)+' successfully completed.\n'
-           print message
-           out_file.write(message)
-	print bar_message
-	out_file.write(bar_message)
+    with open(graphxml, 'r') as file :
+        filedata = file.read()
+    # Replace the target string
+    filedata = filedata.replace('COREGFILE',dimfile)
+    filedata = filedata.replace('IFGFILE', str(ifgdim))
+    filedata = filedata.replace('OUTPUTFOLDER',outputexportfolder)
+    # Write the file out again
+    with open(graph2run, 'w') as file:
+        file.write(filedata)
+
+    args = [ GPT, graph2run, '-c', CACHE, '-q', CPU]
+    print(args)
+    # Launching process
+    process = subprocess.Popen(args, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    timeStarted = time.time()
+    stdout = process.communicate()[0]
+    print('SNAP STDOUT:{}'.format(stdout))
+    timeDelta = time.time() - timeStarted                     # Get execution time.
+    print('['+str(k)+'] Finished process in '+str(timeDelta)+' seconds.')
+    out_file.write('['+str(k)+'] Finished process in '+str(timeDelta)+' seconds.\n')
+    if process.returncode != 0 :
+        message='Error exporting '+str(tail)+'\n'
+        err_file.write(message)
+    else:
+        message='Stamps export of '+str(tail)+' successfully completed.\n'
+    print(message)
+    out_file.write(message)
+    print(bar_message)
+    out_file.write(bar_message)
 out_file.close()
 err_file.close()
